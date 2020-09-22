88fe911e4a6a1034554db0e525c6d916feca82eb
ccamp20 <calista.camp@patelhs.org>
2020-09-22 15:54:37 +0000

    fixed some mistakes and compared my birthday and today's date

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ class CheckMyAge {
     int myAge = 15;
     int myBirthday = 0527;
     int todaysDate = 0922;
-    if(todaysDate == 0922);
+    if(myBirthday == todaysDate);
     {
-    System.out.println ("You are" + intAge);
-    }
+    System.out.println ("You are" + myAge + 1);
+    }else{
+    System.out.println ("You are still" + myAge)
   }
 }
