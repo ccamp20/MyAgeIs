ad3d7caf3d57ca47c32f45287d98722e70b0c60e
ccamp20 <calista.camp@patelhs.org>
2020-09-22 15:49:19 +0000

    wrote in the first half of the if/else statement

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 class CheckMyAge {
   public static void main(String[] args) {
-    int myAge = 15
-    int myBirthday = 0527
-    int todaysDate = 0922
+    int myAge = 15;
+    int myBirthday = 0527;
+    int todaysDate = 0922;
+    if(todaysDate == 0922);
+    {
+    System.out.println ("You are" + intAge);
+    }
   }
 }
